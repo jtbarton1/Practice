a61cb9b6bcc3234066713d0708aa3672201e8dca
Jeremy <jtbarton1@crimson.ua.edu>
2014-05-12 12:56:16 -0700

    Created blank project.

---
+++ WHWriterApp/WHWriterApp/JBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JBAppDelegate.h
+//  WHWriterApp
+//
+//  Created by Jeremy on 5/12/14.
+//  Copyright (c) 2014 CS491. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
